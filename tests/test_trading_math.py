54b261a43c1fcaf601b0c197ba6b4b8662b75e84
AndBa94 <andzejbab@gmail.com>
2026-09-22 15:05:45 +0300

    Fix breakout confirmation fixture

--- tests/test_trading_math.py
+++ tests/test_trading_math.py
@@ -35,7 +35,7 @@ class TradingMathTests(unittest.TestCase):
         for i in range(60):
             close = 100.0
             candles.append(Candle(i, close, close + 0.2, close - 0.2, close, 100))
-        candles[-2] = Candle(58, 100.4, 100.7, 100.2, 100.5, 100)
+        candles[-2] = Candle(58, 100.0, 100.2, 99.9, 100.1, 100)
         candles[-1] = Candle(59, 100.3, 100.8, 100.3, 100.7, 150)
         market = MarketSnapshot("TEST", 100.7, 100.4, 100.6, 10_000_000, 1, 1_000_000, 0, 0, 2)
         orderbook = {
